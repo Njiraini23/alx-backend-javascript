bdec92c6a1511ef9888493b7dc21e197ce7fa7fc
Njiraini23 <njiraini88@gmail.com>
2023-08-07 15:08:16 +0000

    create an object with employee list as return value

---
+++ 0x00-ES6_basic/12-createReportObject2.js
@@ -0,0 +1,14 @@
+export default function createEmployeesObject (employeesList) {
+  const allEmployees = {
+    ...employeesList,
+  };
+
+  const reportObject = {
+    allEmployees,
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+
+  return reportObject;
+}
