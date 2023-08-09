f5d6e5daaf6dac6c3123374d78867ee0808a82d5
Njiraini23 <njiraini88@gmail.com>
2023-08-09 08:54:15 +0000

    return a promise using a prototype

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,11 @@
+/* eslint-disabled */
 function getResponseFromAPI() {
-  return new Promise(resolve, reject} => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const data = { message: "This is the API response" };
+      resolve(data);
+    }, 2000);
   });
+}
+
+export default getResponseFromAPI;
