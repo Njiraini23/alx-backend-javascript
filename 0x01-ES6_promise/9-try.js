cbf57eefcd406b7865d03866bc9968de888897f1
Njiraini23 <njiraini88@gmail.com>
2023-08-10 10:42:01 +0000

    9-try.js

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,14 @@
+export function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    const result = mathFunction();
+    que.push(result);
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}
