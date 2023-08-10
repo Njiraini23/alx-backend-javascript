5ab081abce9f87979b0b3e26a0a19d05f2916c95
Njiraini23 <njiraini88@gmail.com>
2023-08-10 08:05:44 +0000

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,8 +4,8 @@ export default function handleProfileSignup() {
   const p1 = uploadPhoto();
   const p2 = createUser();
 
-return Promise.all([p1, p2]).then((results)) => {
-  console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
-})
-  .catch(() => { console.log('Signup system offline'); });
+  return Promise.all([p1, p2]).then((results) => {
+    console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
+  })
+    .catch(() => { console.log('Signup system offline'); });
 }
---
+++ 0x01-ES6_promise/3-main.js
@@ -0,0 +1,3 @@
+import handProfileSignup from "./3-all";
+
+handProfileSignup();
