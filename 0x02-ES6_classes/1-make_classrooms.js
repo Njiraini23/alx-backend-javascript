8b608709aae2053da470e6ad4468c94ed3cccce2
Njiraini23 <njiraini88@gmail.com>
2023-08-11 11:03:51 +0000

    1-make_classrooms.js

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+import Classroom from './0-classroom.js';
+
+export default function initializeRooms() {
+  const classArray = [
+    newClassRoom(19),
+    newClassRoom(20),
+    newClassRoom(34),
+  ];
+  return classArray;
+}
