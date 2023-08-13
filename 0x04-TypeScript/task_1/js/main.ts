89c09288c5506149b4c047f92f1f853aee0f1b5b
Njiraini23 <njiraini88@gmail.com>
2023-08-13 13:20:02 +0000

    main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,4 +7,44 @@ interface Teacher {
   [key: string]: any
 }
 
-Interface extends Teacher {
+Interface Directors extends Teacher {
+  numberOfReports: number;
+}
+export default Directors;
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher = (firstName: string, lastName: string):string => `${firstName.charAt(0)}. ${lastName}`;
+
+interface StudentClassInterface {
+  firstName: string,
+  lastName: string
+}
+
+interface StudentClassConstructorInterface {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student = newStudentClass('Eric', 'Cantona');
+console.log(student.displayName());
+console.log(student.workOnHomework());
