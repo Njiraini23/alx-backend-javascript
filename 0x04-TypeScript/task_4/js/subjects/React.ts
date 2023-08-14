22bf2d1be56d4a892195e3e916d458f21a3aa703
Njiraini23 <njiraini88@gmail.com>
2023-08-14 18:17:45 +0000

    React.ts

---
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,15 @@
+/// <reference path="Teacher.ts "/>
+/// <reference path="Subjects.ts" />
+
+  namespace Subjects {
+    export interface Teacher {
+      experienceTeachingReact?: number;
+    }
+
+    export class ReactClass extends Subject{
+      getRequirements = (): string => 'Here is the list of requirements for React'
+
+      getAvailableTeacher = (): string => {
+        if (!this.teacher?.experienceTeachingReact) return 'No available teacher';
+
+	return `Available Teacher: ${this.teacher}`;
