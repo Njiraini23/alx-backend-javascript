47c981cfd5c3a4c14a43a860a6cc440871398ea7
Njiraini23 <njiraini88@gmail.com>
2023-08-14 15:23:00 +0000

    main.ts

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,13 @@
+import { RowId, RowElement } from './interface';
+import * as CRUD from './crud';
+
+const row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+}
+
+const newRowID: RowId = CRUD.insertRow(row);
+const updatedRow: RowElement = { ...row, age:23};
+
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
