b32d54df0f5945dbb16a04045f935be647dbb8ce
Njiraini23 <njiraini88@gmail.com>
2023-08-24 11:30:35 +0000

    linting

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,16 +1,16 @@
-export default function cleanSet(set startString) {
+export default function cleanSet(set, startString) {
   if (!startString || startString.length === 0) {
     return '';
   }
 
-// the set of arrays
-const arrayFromSet = Array.from(set);
+  // the set of arrays
+  const arrayFromSet = Array.from(set);
 
-const filterAndModifiedValues = arrayFromSet
-  .filter((value) => value !== undefined && value.startsWith(startString))
-  .map((value) => value.slice(startString.length));
+  const filteredAndModifiedValues = arrayFromSet
+    .filter((value) => value !== undefined && value.startsWith(startString))
+    .map((value) => value.slice(startString.length));
 
-const resultString = filteredAndModifiedValues.join('-');
+  const resultString = filteredAndModifiedValues.join('-');
 
-return resultString;
+  return resultString;
 }
