644b778b9b5d147e48e124771c1961a7337805a5
Njiraini23 <njiraini88@gmail.com>
2023-08-24 11:25:40 +0000

    8-clean_set.js

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,16 @@
+export default function cleanSet(set startString) {
+  if (!startString || startString.length === 0) {
+    return '';
+  }
+
+// the set of arrays
+const arrayFromSet = Array.from(set);
+
+const filterAndModifiedValues = arrayFromSet
+  .filter((value) => value !== undefined && value.startsWith(startString))
+  .map((value) => value.slice(startString.length));
+
+const resultString = filteredAndModifiedValues.join('-');
+
+return resultString;
+}
