831fe69fa56c1769c92fe3e8c084e792c89d48c4
Njiraini23 <njiraini88@gmail.com>
2023-10-01 13:21:52 +0000

    1-stdin.js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.setEncodig('utf8');
+
+process.stdin.on('readable', () => {
+  const INPUT = process.stdin.read();
+  if (INPUT) process.stdout.write(`Your name is: ${INPUT}`);
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
