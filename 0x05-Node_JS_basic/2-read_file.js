a5707206c6822b52a641685c67f41d5702b838f7
Njiraini23 <njiraini88@gmail.com>
2023-10-01 18:24:05 +0000

    fixes

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -10,24 +10,24 @@ function countStudents(fileName) {
     for (let i = 0; i < lines.length; i += 1) {
       if (lines[i]) {
         length += 1;
-	const field = lines[i].toString().split(',');
-	if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-	  students[field[3]].push(field[0]);
-	} else {
-	  students[field[3]] = [field[0]];
-	}
-	if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
-	  fields[field[3]] += 1;
-	} else {
-	  fields[field[3]] = 1;
-	}
+        const field = lines[i].toString().split(',');
+        if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+          students[field[3]].push(field[0]);
+        } else {
+          students[field[3]] = [field[0]];
+        }
+        if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+          fields[field[3]] += 1;
+        } else {
+          fields[field[3]] = 1;
+        }
       }
     }
     const l = length - 1;
     console.log(`Number of students: ${l}`);
     for (const [key, value] of Object.entries(fields)) {
       if (key !== 'field') {
-	console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
+        console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
       }
     }
   } catch (error) {
@@ -36,4 +36,3 @@ function countStudents(fileName) {
 }
 
 module.exports = countStudents;
-
